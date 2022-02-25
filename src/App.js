35625f03504bd4d6c34982df321aa03d3bb75754
Sean Thornber <Sean.Thornber@godeploy.com>
2022-02-25 16:12:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      originalTime: 1500,
+      time: 1500,
+      active: true
+    }
+
+    this.timerFunc = this.timerFunc.bind(this)
+  }
+
+  timerFunc = () =>{
+    setTimeout(() => {
+    this.setState({
+      time: this.state.time - 1
+    });
+    if (this.state.time > 0) {
+      this.timerFunc();
+    }}, 1000)
+  }
+
+  render() {
+    let x = 83;
+    console.log(Math.floor(x / 60));
+    console.log(x % 60);
+
+    return (
+      <div id="top-level-container">
+
+        <div id="display">
+          <div id="controls">
+            <div id="break-controls" className="control-div">
+              <h1 id="break-label">Break Length</h1>
+              <div className="control-buttons-div">
+                <a><h1>+</h1></a>
+                <h1>5</h1>
+                <a><h1>-</h1></a>
+              </div>
+            </div>
+
+            <div id="session-controls" className="control-div">
+              <h1 id="session-label">Session Length</h1>
+              <div className="control-buttons-div">
+                <a><h1>+</h1></a>
+                <h1>25</h1>
+                <a><h1>-</h1></a>
+              </div>
+            </div>
+          </div>
+
+          <div id="clock-display">
+            <h1 id="timer">{Math.floor(this.state.time / 60) + ":" + this.state.time % 60}</h1>
+          </div>
+
+          <div id="start-stop-reset-controls">
+            <a onClick={this.timerFunc}><i class="bi bi-play"></i></a>
+            <i class="bi bi-pause"></i>
+            <i class="bi bi-stop"></i>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
